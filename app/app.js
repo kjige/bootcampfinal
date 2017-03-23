d548dbf210b1265c491ea36925d01de114fa1246
poornima sewak <poornima.mathur.20@gmail.com>
2017-03-22 20:44:11 -0500

    Forum, Forum form, suggestioncomponent

--- app/app.js
+++ app/app.js
@@ -14,6 +14,7 @@ import { FreelancerSignUpForm } from "./components/children/FreelancerSignUpForm
 import { EmployerSignUpForm } from "./components/children/EmployerSignUpForm";
 import { Login } from "./components/Login";
 import { Register } from "./components/Register";
+import { Forum } from "./components/Forum";
 
 const router = (
     <Router history={hashHistory}>
@@ -29,6 +30,7 @@ const router = (
 
             <Route path='login' component={Login} />
             <Route path='register' component={Register} />
+            <Route path='forum' component={Forum} />
 
             <IndexRoute component={Home} />
         </Route>
