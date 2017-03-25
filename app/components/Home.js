8923a8f773173061876faf81b580565a35034407
poornima sewak <poornima.mathur.20@gmail.com>
2017-03-25 12:07:03 -0500

    code not to bother

--- app/components/Home.js
+++ app/components/Home.js
@@ -21,21 +21,12 @@ class Home extends React.Component {
       <div className='container'>
           <div className="row">
               <div className="box">Home
-                {/*<div class="carousel">
-                    <div class="item a">A</div>
-                    <div class="item b">B</div>
-                    <div class="item c">C</div>
-                    <div class="item d">D</div>
-                    <div class="item e">E</div>
-                    <div class="item f">F</div>
-                </div>
-
-                <div class="next">Next</div>
-                <div class="prev">Prev</div>*/}
+               
               </div>
           </div>
       </div>
     );
+
   }
 }
 
