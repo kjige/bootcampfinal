91cce5ca58f0ccf943c9a5b1c612088ffb5f37b3
poornima sewak <poornima.mathur.20@gmail.com>
2017-03-27 14:27:47 -0500

    style

--- app/components/children/grandchildren/EachEmployer.js
+++ app/components/children/grandchildren/EachEmployer.js
@@ -3,18 +3,18 @@ import * as React from 'react';
 class EachEmployer extends React.Component {
   render() {
     return(
-      <div>
-        <div className='col-xs-6'>
+      <div className='row-ls-12'>
+        {/*<div className='col-xs-6 border-img'>
           <h6>Name</h6>
           <h6>Company</h6>
           <h6>Field needed</h6>
           <h6>Description</h6>
-        </div>
-        <div className='col-xs-6'>
+        </div>*/}
+        <div className='col-ls-12 border-img'>
           <h6>{this.props.name}</h6>
-          <h6>{this.props.company}</h6>
-          <h6>{this.props.fieldNeeded}</h6>
-          <h6>{this.props.description}</h6>
+          <h6>Is working in : {this.props.company}</h6>
+          <h6>Can help with : {this.props.fieldNeeded}</h6>
+          <h6>About me : {this.props.description}</h6>
         </div>
       </div>
     )
