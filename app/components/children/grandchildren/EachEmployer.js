4faef6436064d8a09e99bf2963d6efa10223546d
poornima sewak <poornima.mathur.20@gmail.com>
2017-03-29 20:21:22 -0500

    react front end polishing

--- app/components/children/grandchildren/EachEmployer.js
+++ app/components/children/grandchildren/EachEmployer.js
@@ -6,7 +6,7 @@ class EachEmployer extends React.Component {
       <div className='row-ls-12'>
        <div className='col-ls-12 border-img emp'>
           <img src={this.props.image} width='100' height='100' className='img-circle avatar'/>
-          <h5>{this.props.name}</h5>
+          <h5 className='sty'>{this.props.name}</h5>
           <h6>Part of : {this.props.company}</h6>
           <h6>Need help with : {this.props.fieldNeeded}</h6>
           <h6>About us : {this.props.description}</h6>
--- app/components/children/grandchildren/EachFreelancerProfile.js
+++ app/components/children/grandchildren/EachFreelancerProfile.js
@@ -6,7 +6,7 @@ class EachFreelancerProfile extends React.Component {
       <div className='row-xs-12'>
        <div className='col-xs-12 border-img emp'>
           <img src={this.props.image} width='100' height='100' className='img-circle avatar'/>
-          <h5>{this.props.name}</h5>
+          <h5 className='sty'>{this.props.name}</h5>
           <h6>Working in : {this.props.field}</h6>
           <h6>Has : {this.props.experience} yrs of experience.</h6>
         </div>
